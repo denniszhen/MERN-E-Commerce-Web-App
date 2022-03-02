bbd03e695a7259d72abb1e5437dfaa538fd9bdd4
Dennis Zhen <54695660+denniszhen@users.noreply.github.com>
2022-03-01 23:44:03 -0500

    Implemented Category Tiles

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -4,14 +4,12 @@ const Container = styled.div`
     flex: 1;
     margin: 3px;
     height: 70vh; 
+    position: relative;
 `
 
-const Title = styled.div`
-    display: flex;
-    padding: 20px;
-    border: 1px solid black;
-    justify-content: center;
-    align-text: center; 
+const Title = styled.h1`
+    color: white;
+    margin-bottom: 20px; 
 `
 const Image = styled.img`
     width: 100%;
@@ -19,11 +17,25 @@ const Image = styled.img`
     object-fit: cover;
 `
 const Button = styled.button`
-    flex: 1;
-    width: 20;
-    align-text: center;
+    border: none;
+    padding: 10px;
+    background-color: white;
+    color: gray;
+    font-weight: 600; 
+    cursor: pointer;
 `
 
+const Info = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0; 
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center; 
+`
 
 const CategoryItem = ({item}) => {
     return (
